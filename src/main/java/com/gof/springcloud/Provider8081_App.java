4f222b6a3c835f15104c2494a0976c94e22afcc2
SUN SHUN <sunshun18@126.com>
2021-05-05 21:46:01 +0800

    import nacos (service register)

--- src/main/java/com/gof/springcloud/Provider8081_App.java
+++ src/main/java/com/gof/springcloud/Provider8081_App.java
@@ -2,9 +2,10 @@ package com.gof.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class Provider8081_App
 {
 	public static void main(String[] args)
